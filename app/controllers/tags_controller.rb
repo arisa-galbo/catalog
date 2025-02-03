f01d6e20daa326e7cb85d01e61b89aa6bb413c76
Arisa Miyashita <miyashitaarisa@gmail.com>
2025-02-03 07:45:50 +0000

    [feature] tagのCRUD機能の作成

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,49 @@
 class TagsController < ApplicationController
+    allow_unauthenticated_access only: %i[ index show ]
+    before_action :set_tag, only: %i[show edit update destroy]
     def index
         @tags = Tag.all
+    end
+    
+    def show
+    end
+  
+    def new
+      @tag = Tag.new
+    end
+  
+    def create
+      @tag = Tag.new(tag_params)
+      if @tag.save
+        redirect_to tags_path, notice: "タグを作成しました！"
+      else
+        render :new, status: :unprocessable_entity
       end
+    end
+  
+    def edit
+    end
+  
+    def update
+      if @tag.update(tag_params)
+        redirect_to tags_path, notice: "タグを更新しました！"
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @tag.destroy
+      redirect_to tags_path, notice: "タグを削除しました！"
+    end
+  
+    private
+  
+    def set_tag
+      @tag = Tag.find(params[:id])
+    end
+  
+    def tag_params
+      params.require(:tag).permit(:name)
+    end
 end
