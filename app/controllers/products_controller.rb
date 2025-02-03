fd05208d9fb2f54cba4985f78c56a8a401dee29f
Arisa Miyashita <miyashitaarisa@gmail.com>
2025-02-03 05:56:25 +0000

    [feature]productの詳細ページを追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,23 @@
 class ProductsController < ApplicationController
+    allow_unauthenticated_access only: %i[ show ]
+    before_action :set_brand
+    before_action :set_product, only: [:show]
+    def create
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.create(comment_params)
+        redirect_to article_path(@article)
+    end
+
+    def show
+    end
+
+    private
+  
+    def set_brand
+      @brand = Brand.find(params[:brand_id])
+    end
+  
+    def set_product
+      @product = @brand.products.find(params[:id])
+    end
 end
