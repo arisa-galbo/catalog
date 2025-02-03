030727259b6206506e58621ecf14f0624ee4c51e
Arisa Miyashita <miyashitaarisa@gmail.com>
2025-02-03 03:48:06 +0000

    [feature]ブランド新規登録機能作成

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -6,4 +6,22 @@ class BrandsController < ApplicationController
   def show
     @brand = Brand.find(params[:id])
   end
+
+  def new
+    @brand = Brand.new
+  end
+
+  def create
+    @brand = Brand.new(brand_params)
+    if @brand.save
+      redirect_to @brand
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def brand_params
+    params.expect(brand: [ :name ])
+  end
 end
